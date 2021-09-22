42ce90d5a90a24bb1f4e21abb5544e20eba56ad1
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-09-22 19:31:20 +0300

    Initial Commit

---
+++ ContactsApp SwiftUI/ContactsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp_SwiftUIApp.swift
+//  ContactsApp SwiftUI
+//
+//  Created by Karpinets Alexander on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
